36802a9358627c182787fa792e5218d2ed34c1c3
Kyrad1 <flealsaez@gmail.com>
2024-09-26 03:02:12 -0300

    mejora del menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,77 @@
-import { IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import {settings} from 'ionicons/icons'
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
+    <>
+      <IonMenu contentId="main-content">
+        <IonHeader >
+          <IonToolbar color="btoncolor">
+            <IonTitle><p>Menú</p></IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding" color="bgcolor">
+          <IonList>
+            <IonItem>
+              <IonLabel><p>Perfil</p></IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel><p>Plan de entrenamiento</p></IonLabel>
+            </IonItem>
+            
+            <IonItem>
+              <IonLabel><p>Soporte</p></IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel><p>Configuracion</p></IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+      <IonPage id = "main-content" >
+        <IonHeader className='menuHeader'>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle><p>Menu</p></IonTitle>
+          </IonToolbar>
+        </IonHeader>
         
-      </IonHeader>
-      <IonContent color='bgcolor' >
-      <div className="cardContainer">
-        <IonCard color='btoncolor' href='Quest'>
-          <IonCardContent class='buttonMenuText'>
-          <p className='text'>QUEST!</p>
-          </IonCardContent>
-        </IonCard>
-      </div>
-      <div className='nombreLvlClass'>
-      <p>Kyrad/lvl 1/Guerrero</p>
-      </div>
-      
-      <div className='imageContainer'>
-        <img src='/public/img/warriorIdle.png' className="custom-image"/>
-      </div>
-      <div className="cardContainer">
-        <IonCard color='btoncolor'>
-          <IonCardContent class='buttonMenuText'>
-          <p className='text'>WORKOUT PLAN</p>
-          </IonCardContent>
-        </IonCard>
-      </div>
-      <div className="cardSettingsContainer">
-        <IonCard className='settingsCard' color='btoncolor'>
-          <IonCardContent class='buttonMenuText'>
-            <IonIcon icon={settings} size='large'/>
-          </IonCardContent>
-        </IonCard>
-      </div>
-      </IonContent>
-    </IonPage>
+        <IonContent color="bgcolor">
+        
+        <div className='campfireImageDiv'>
+          <div className="light-effect"></div>
+          <img  src='https://media.tenor.com/drxH1lO9cfEAAAAi/dark-souls-bonfire.gif'/>
+        </div>    
+        <div className="cardContainer" >
+          <IonCard href='/quest'  color='btoncolor' className='cardButton'>
+            <IonCardContent class='buttonMenuText'>
+            <p className='text2'>QUEST!</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
+        <div className="cardContainer">
+          <IonCard color='btoncolor' className='cardButton'>
+            <IonCardContent class='buttonMenuText'>
+            <p className='text'>Crea tu plan de entrenamiento</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
+        
+        <div className="cardContainer">
+          <IonCard color='btoncolor' className='cardButton'>
+            <IonCardContent class='buttonMenuText'>
+            <p className='text2'>Status</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
+        
+        </IonContent>
+        
+      </IonPage>
+    </>
   );
 };
 
--- src/pages/Quest.tsx
+++ src/pages/Quest.tsx
@@ -1,6 +1,7 @@
-import { IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Quest.css';
-import {alertCircle} from 'ionicons/icons'
+import {alertCircle,arrowBackCircle,barbell,checkmark,chevronDown,chevronUp, stopwatch, timer} from 'ionicons/icons'
+
 
 
 const Quest: React.FC = () => {
@@ -11,10 +12,53 @@ const Quest: React.FC = () => {
       </IonHeader>
       <IonContent color='bgcolor' >
         <div className='barraSuperior'>
-          <IonIcon icon={alertCircle} size='large' color='light' />
+          <IonIcon icon={alertCircle}className='alertIcon' />
            <p className='textStyle'>Quest Info</p>
+            <IonCard href='/home' className='backButton'><IonIcon icon={arrowBackCircle} className='backIcon'></IonIcon></IonCard>
+        </div>
+        <div className='quest1Message'>  
+          <p>DAILY QUEST -</p> 
+        </div>
+        <div className='quest2Message'>  
+          <p>Recuerda: cada sentadilla te acerca un paso más al Super Saiyajin.</p> 
         </div>
 
+        <div className='barraGoals'>
+          <IonIcon icon={barbell} className='dumbbellIcon'></IonIcon>
+          <p className='textStyleGoals'>goals</p>
+          <IonIcon icon={barbell} className='dumbbellIcon2'></IonIcon>
+        </div>
+        <div className='excerciseBarra'>
+          <p className='excerciseText'>Flexiones</p>
+          <p className='excerciseSetNumber'>3/3</p>
+          <div className='plusButton'><IonIcon icon={chevronUp} size='large'></IonIcon></div>
+          <div className='minusButton'><IonIcon icon={chevronDown} size='large'></IonIcon></div>
+          <div className='checkButton'><IonIcon className='checkIcon' icon={checkmark} size='large'></IonIcon></div>
+        </div>
+        <div className='excerciseBarra'>
+          <p className='excerciseText'>Sentadillas</p>
+          <p className='excerciseSetNumber'>3/3</p>
+          <div className='plusButton'><IonIcon icon={chevronUp} size='large'></IonIcon></div>
+          <div className='minusButton'><IonIcon icon={chevronDown} size='large'></IonIcon></div>
+          <div className='checkButton'><IonIcon className='checkIcon' icon={checkmark} size='large'></IonIcon></div>
+        </div>
+        <div className='excerciseBarra'>
+          <p className='excerciseText'>Abdominales</p>
+          <p className='excerciseSetNumber'>2/3</p>
+          <div className='plusButton'><IonIcon icon={chevronUp} size='large'></IonIcon></div>
+          <div className='minusButton'><IonIcon icon={chevronDown} size='large'></IonIcon></div>
+          <div className='checkButton'><IonIcon className='checkIcon' icon={checkmark} size='large'></IonIcon></div>
+        </div>
+        <div>  
+          <p className='caution'>CAUTION!</p> 
+          <p className='cautionMessage'>Si fallas en completar la mision krillin morira</p>
+        </div>
+        <div className='restTime'><p className='time'>Rest Time: 60</p></div>
+        <div className='restTimeIcon'><IonIcon className='timerIcon' icon={stopwatch}></IonIcon></div>
+
+
+
+        
             
       </IonContent>
     </IonPage>
